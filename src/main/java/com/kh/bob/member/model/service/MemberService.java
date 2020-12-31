fef85e2daba6ba92e3b66075902725407b4f61bb
user <user@VMW-Win10>
2020-12-31 23:03:30 +0900

    test

--- src/main/java/com/kh/bob/member/model/service/MemberService.java
+++ src/main/java/com/kh/bob/member/model/service/MemberService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.kh.bob.member.model.service;
 
 import org.springframework.stereotype.Service;
@@ -7,13 +6,3 @@ import org.springframework.stereotype.Service;
 public interface MemberService {
 
 }
-=======
-package com.kh.bob.member.model.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface MemberService {
-
-}
->>>>>>> c8a4f49635a283fbf432c5b0e6a2314660b9d212
